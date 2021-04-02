9b5990529326ed0ac1e20fd68f797bca0aeb9df3
patilharshal2020 <78806133+patilharshal2020@users.noreply.github.com>
2021-04-02 23:46:53 +0530

    Add files via upload

---
+++ project-main/assets/script.js
@@ -0,0 +1,15 @@
+
+
+function success() {
+    confirm('Confirm your details')
+    alert('Your request submited successfully')
+}
+
+
+function chImg(src) {
+    document.getElementById('bigImg').src = src;
+}
+
+function chImgSm(src) {
+    document.getElementById('bigImg-sm').src = src;
+}
